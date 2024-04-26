5d2b8853899c4e9f137823338619a3183e07b011
dmitriibaikov <bdv@empayre.com>
2024-04-26 15:57:59 +0300

    FIN-29: Init commit [fix after review A77AY]

--- proto/dominator.thrift
+++ proto/dominator.thrift
@@ -10,57 +10,76 @@ exception BadContinuationToken { 1: string reason }
 exception LimitExceeded {}
 
 struct CommonSearchQueryParams {
-    1: optional list<string> currencies
-    2: optional ContinuationToken continuation_token
-    3: optional i32 limit
+    1: optional list<base.EventID> ids
+    2: optional list<string> currencies
+    3: optional ContinuationToken continuation_token
+    4: optional i32 limit
 }
 
 /* Набор параметров для поиска условий магазинов */
-struct ShopsSearchQuery {
+struct ShopSearchQuery {
     1: required CommonSearchQueryParams common_search_query_params
-    2: required domain.PartyID party_id
+    2: optional domain.PartyID party_id
     3: optional list<domain.ShopID> shop_ids
     4: optional list<string> term_sets_names
 }
 
 /* Набор параметров для поиска условий кошельков */
-struct WalletsSearchQuery {
+struct WalletSearchQuery {
     1: required CommonSearchQueryParams common_search_query_params
-    2: required domain.PartyID party_id
+    2: optional domain.PartyID party_id
     3: optional list<base.ID> identity_ids
     4: optional list<string> term_sets_names
 }
 
 /* Набор параметров для поиска условий терминалов */
-struct TerminalsSearchQuery {
+struct TerminalSearchQuery {
     1: required CommonSearchQueryParams common_search_query_params
     2: optional list<base.ID> provider_ids
     3: optional list<base.ID> terminal_ids
 }
 
-/* Набор условий для магазина/кошелька */
-struct TermSet {
+/* Набор условий для магазина */
+struct ShopTermSet {
     1: required domain.PartyID owner_id
     2: optional domain.ShopID shop_id
-    3: optional domain.IdentityProviderRef identity_id
     4: required domain.ContractID contract_id
     5: required string shop_name
     6: required string term_set_name
-    7: required domain.TermSetHierarchyObject current_term_set
-    8: optional list<domain.TermSetHierarchyObject> term_set_history
+    7: required string currency
+    8: required domain.TermSetHierarchyObject current_term_set
+    9: optional list<domain.TermSetHierarchyObject> term_set_history
+}
+
+/* Набор условий для кошелька */
+struct WalletTermSet {
+    1: required domain.PartyID owner_id
+    3: optional domain.IdentityProviderRef identity_id
+    4: required domain.ContractID contract_id
+    6: required string term_set_name
+    7: required string currency
+    8: required domain.TermSetHierarchyObject current_term_set
+    9: optional list<domain.TermSetHierarchyObject> term_set_history
 }
 
 /* Набор условий для терминала */
 struct TerminalTermSet {
     1: required domain.TerminalRef terminal_id
     2: required string terminal_name
-    3: required string provider
-    4: required domain.ProvisionTermSet current_term_set
-    5: optional list<domain.ProvisionTermSet> term_set_history
+    3: required domain.ProviderRef provider_id
+    4: required string provider_name
+    5: required string currency
+    6: required domain.ProvisionTermSet current_term_set
+    7: optional list<domain.ProvisionTermSet> term_set_history
 }
 
-struct TermSetsResponse {
-    1: required list<TermSet> terms
+struct ShopTermSetsResponse {
+    1: required list<ShopTermSet> terms
+    2: optional string continuation_token
+}
+
+struct WalletTermSetsResponse {
+    1: required list<WalletTermSet> terms
     2: optional string continuation_token
 }
 
@@ -72,14 +91,14 @@ struct TerminalTermSetsResponse {
 service DominatorService {
 
     /* Поиск условий для магаизнов */
-    TermSetsResponse SearchShopTermSets (1: ShopsSearchQuery shops_search_query)
+    ShopTermSetsResponse SearchShopTermSets (1: ShopSearchQuery shop_search_query)
         throws (1: BadContinuationToken ex1, 2: LimitExceeded ex2, 3: base.InvalidRequest ex3)
 
     /* Поиск условий для кошельков */
-    TermSetsResponse SearchWalletTermSets (1: WalletsSearchQuery wallets_search_query)
+    WalletTermSetsResponse SearchWalletTermSets (1: WalletSearchQuery wallet_search_query)
         throws (1: BadContinuationToken ex1, 2: LimitExceeded ex2, 3: base.InvalidRequest ex3)
 
     /* Поиск условий для терминалов */
-    TerminalTermSetsResponse SearchTerminalTermSets (1: TerminalsSearchQuery terminals_search_query)
+    TerminalTermSetsResponse SearchTerminalTermSets (1: TerminalSearchQuery terminal_search_query)
         throws (1: BadContinuationToken ex1, 2: LimitExceeded ex2, 3: base.InvalidRequest ex3)
 }
