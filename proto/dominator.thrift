184e2d75dc4c638792da41788aff5838b994a7e1
dmitriibaikov <bdv@empayre.com>
2024-04-26 13:06:49 +0300

    FIN-29: Init commit [fix after review]

--- proto/dominator.thrift
+++ proto/dominator.thrift
@@ -15,65 +15,71 @@ struct CommonSearchQueryParams {
     3: optional i32 limit
 }
 
+/* Набор параметров для поиска условий магазинов */
 struct ShopsSearchQuery {
     1: required CommonSearchQueryParams common_search_query_params
     2: required domain.PartyID party_id
     3: optional list<domain.ShopID> shop_ids
-    4: optional list<string> term_names
+    4: optional list<string> term_sets_names
 }
 
+/* Набор параметров для поиска условий кошельков */
 struct WalletsSearchQuery {
     1: required CommonSearchQueryParams common_search_query_params
     2: required domain.PartyID party_id
     3: optional list<base.ID> identity_ids
-    4: optional list<string> term_names
+    4: optional list<string> term_sets_names
 }
 
+/* Набор параметров для поиска условий терминалов */
 struct TerminalsSearchQuery {
     1: required CommonSearchQueryParams common_search_query_params
     2: optional list<base.ID> provider_ids
     3: optional list<base.ID> terminal_ids
 }
 
-struct Term {
+/* Набор условий для магазина/кошелька */
+struct TermSet {
     1: required domain.PartyID owner_id
     2: optional domain.ShopID shop_id
     3: optional domain.IdentityProviderRef identity_id
     4: required domain.ContractID contract_id
     5: required string shop_name
-    6: required string term_name
-    7: required domain.TermSetHierarchyObject current_term
-    8: optional list<domain.TermSetHierarchyObject> term_history
+    6: required string term_set_name
+    7: required domain.TermSetHierarchyObject current_term_set
+    8: optional list<domain.TermSetHierarchyObject> term_set_history
 }
 
-struct TerminalTerm {
+/* Набор условий для терминала */
+struct TerminalTermSet {
     1: required domain.TerminalRef terminal_id
     2: required string terminal_name
     3: required string provider
-    4: required domain.ProvisionTermSet current_term
-    5: optional list<domain.ProvisionTermSet> term_history
+    4: required domain.ProvisionTermSet current_term_set
+    5: optional list<domain.ProvisionTermSet> term_set_history
 }
 
-struct TermsResponse {
-    1: required list<Term> terms
+struct TermSetsResponse {
+    1: required list<TermSet> terms
     2: optional string continuation_token
 }
 
-struct TerminalTermsResponse {
-    1: required list<TerminalTerm> terms
+struct TerminalTermSetsResponse {
+    1: required list<TerminalTermSet> terms
     2: optional string continuation_token
 }
 
 service DominatorService {
 
-    TermsResponse SearchShopTerms (1: ShopsSearchQuery shops_search_query)
+    /* Поиск условий для магаизнов */
+    TermSetsResponse SearchShopTermSets (1: ShopsSearchQuery shops_search_query)
         throws (1: BadContinuationToken ex1, 2: LimitExceeded ex2, 3: base.InvalidRequest ex3)
 
-
-    TermsResponse SearchWalletTerms (1: WalletsSearchQuery wallets_search_query)
+    /* Поиск условий для кошельков */
+    TermSetsResponse SearchWalletTermSets (1: WalletsSearchQuery wallets_search_query)
         throws (1: BadContinuationToken ex1, 2: LimitExceeded ex2, 3: base.InvalidRequest ex3)
 
-
-    TerminalTermsResponse SearchTerminalTerms (1: TerminalsSearchQuery terminals_search_query)
+    /* Поиск условий для терминалов */
+    TerminalTermSetsResponse SearchTerminalTermSets (1: TerminalsSearchQuery terminals_search_query)
         throws (1: BadContinuationToken ex1, 2: LimitExceeded ex2, 3: base.InvalidRequest ex3)
 }
